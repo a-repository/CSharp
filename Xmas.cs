682d37d62a8b989fd8d39f0e0a76c17ebd49d261
r-rezaee <31726186+r-rezaee@users.noreply.github.com>
2017-10-29 22:52:00 +0100

    Second Commit
    Author added

--- Xmas.cs
+++ Xmas.cs
@@ -3,37 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// Printing out an Xmas Tree 
+// Author: Reza Rezaee
 
-namespace EDX_Assign_2
+
+namespace Xmas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int rowSize = 8;
-            int colSize = 8;
-            var board = "";
-
-            for (int row = 0; row < rowSize; row++)
-            {
-                for (int col = 0; col < colSize; col++)
-                {
-                    if ((row + col) % 2 == 0)
-                    {
-                        board += " X ";
-                    }
-                    else
-                    {
-                        board += " # ";
-                    }
-                }
-                board += "\n";
-            }
-
-            // Skriva ut Chess board
-            Console.WriteLine(board + "\n");
-
-
             // Skriva ut en gran
 
             // Defineras alla variablar här
@@ -80,7 +59,5 @@ namespace EDX_Assign_2
             // Skriva ut granen
             Console.WriteLine(board2);
         }
-        
-        
     }
 }
